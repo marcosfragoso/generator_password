b89a9deb839440fdf1d24c2b26fca85e667f8305
Marcos <fragoso.ufcg@gmail.com>
2023-03-18 10:25:55 -0300

    upgrade project

--- projeto_generator_password/app_generator_password/views.py
+++ projeto_generator_password/app_generator_password/views.py
@@ -17,7 +17,13 @@ def passwords(request):
 
     length_password = int(request.GET.get('number_characters'))
 
-    password = "".join(random.sample(to_generate, length_password))
-
+    if length_password == 0:
+        password = "There are no passwords with 0 characters.. Please try again!"
+    elif length_password < 0:
+        password = "There are no passwords with negative characters. Please try again!"
+    elif length_password < 70:
+        password = "".join(random.sample(to_generate, length_password))
+    else:
+        password = "The password is too long. Please try again!"
 
     return render(request, 'generators/password.html', {"senha": password})
